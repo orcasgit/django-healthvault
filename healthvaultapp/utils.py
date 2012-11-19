4571055ce75b29a6d4c4f02e268fcec3b3523f38
Rebecca Lovewell <rebecca@caktusgroup.com>
2012-11-19 14:08:06 -0500

    refs #3, #7 - Use HealthVaultConn.authorization_url; added docs

--- healthvaultapp/utils.py
+++ healthvaultapp/utils.py
@@ -1,10 +1,37 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
+from healthvaultlib.healthvault import HealthVaultConn
+
 from . import defaults
+from .models import HealthVaultUser
+
+
+def create_connection(wctoken=None, **kwargs):
+    """Shortcut to create a HealthVault connection instance.
+
+    Configuration parameters default to those defined in the project settings.
+    """
+    config = {
+        'app_id': get_setting('HEALTHVAULT_APP_ID'),
+        'app_thumbprint': get_setting('HEALTHVAULT_THUMBPRINT'),
+        'public_key': get_setting('HEALTHVAULT_PUBLIC_KEY'),
+        'private_key': get_setting('HEALTHVAULT_PRIVATE_KEY'),
+        'server': get_setting('HEALTHVAULT_SERVER'),
+        'shell_server': get_setting('HEALTHVAULT_SHELL_SERVER'),
+    }
+    config.update(kwargs)
+    return HealthVaultConn(wctoken=wctoken, **config)
 
 
 def get_setting(name, use_defaults=True):
+    """Retrieves the specified setting from the project settings file.
+
+    If the setting is not found and ``use_defaults`` is ``True``, then the
+    default value specified in ``defaults.py`` is used.
+
+    :raises: ``ImproperlyConfigured`` if the setting is not found.
+    """
     if hasattr(settings, name):
         return getattr(settings, name)
     if use_defaults:
@@ -12,3 +39,16 @@ def get_setting(name, use_defaults=True):
             return getattr(defaults, name)
     msg = "{0} must be specified in your settings".format(name)
     raise ImproperlyConfigured(msg)
+
+
+def is_integrated(user):
+    """Returns ``True`` if we have HealthVault authentication data for the
+    user.
+
+    This does not require that the authentication data is valid.
+
+    :param user: A Django user.
+    """
+    if user.is_authenticated() and user.is_active:
+        return HealthVaultUser.objects.filter(user=user).exists()
+    return False
--- healthvaultapp/views.py
+++ healthvaultapp/views.py
@@ -10,7 +10,21 @@ from .models import HealthVaultUser
 
 @login_required
 def authorize(request):
-    # Store the redirect URL in the session for after authorization completion
+    """
+    Begins the HealthVault authentication process by redirecting the user to
+    a URL at which they can authorize us to access their HealthVault account.
+
+    When the user has finished at the HealthVault site, they will be
+    redirected to the :py:func:`healthvaultapp.views.complete` view.
+
+    If 'next' is provided in the GET data, it is saved in the session so the
+    :py:func:`healthvaultapp.views.complete` view can redirect the user to
+    that URL upon successful authorization.
+
+    URL name:
+        `healthvault-authorize`
+    """
+    # Store the redirect URL in the session for after authorization completion.
     next_url = request.GET.get('next', None)
     if next_url:
         request.session['healthvault_next'] = next_url
@@ -22,25 +36,42 @@ def authorize(request):
     callback_url = request.build_absolute_uri(reverse('healthvault-complete'))
 
     # Build the authorization URL.
-    server = utils.get_setting('HEALTHVAULT_SHELL_SERVER')
-    app_id = utils.get_setting('HEALTHVAULT_APP_ID')
-    target = 'APPAUTH'
-    targetqs = urlencode({'appid': app_id, 'redirect': callback_url})
-    params = urlencode({'target': target, 'targetqs': targetqs})
-    authorization_url = 'https://{0}/redirect.aspx?{1}'.format(server, params)
+    conn = utils.create_connection()
+    authorization_url = conn.authorization_url(callback_url)
 
     return redirect(authorization_url)
 
 
 @login_required
 def complete(request):
+    """
+    After the user authorizes us, HealthVault sends a callback to this URL so
+    that we can complete integration.
+
+    If there was an error, the user is redirected again to the
+    `healthvault-error` view.
+
+    If the authorization was successful, the credentials are stored for us to
+    use later, and the user is redirected. If 'healthvault_next' is in the
+    request session, the user is redirected to that URL. Otherwise, they are
+    redirected to the URL specified by the setting
+    :ref:`HEALTHVAULT_AUTHORIZE_REDIRECT`.
+
+    URL name:
+        `healthvault-complete`
+    """
+    # Break down the request to get authorization data.
     token = request.GET.get('wctoken', None)
     if not token:
         request.session.pop('healthvault_next', None)
-        return redirect(reverse('healthvault_error'))
+        return redirect(reverse('healthvault-error'))
+
+    # Save the user's authorization information.
     hvuser, _ = HealthVaultUser.objects.get_or_create(user=request.user)
     hvuser.token = token
     hvuser.save()
+
+    # Redirect the user to the stored redirect URL or default.
     next_url = request.session.pop('healthvault_next', None)
     if not next_url:
         next_url = utils.get_setting('HEALTHVAULT_AUTHORIZE_REDIRECT')
@@ -79,6 +110,15 @@ def error(request, extra_context=None):
 
 @login_required
 def deauthorize(request):
+    """Forget the user's HealthVault credentials.
+
+    If the request has a `next` parameter, the user is redirected to that URL.
+    Otherwise, they are redirected to the URL defined by the setting
+    :ref:`HEALTHVAULT_DEAUTHORIZE_REDIRECT`.
+
+    URL name:
+        `healthvault-deauthorize`
+    """
     # TODO: is there a way to self-deauthorize from healthvault?
     HealthVaultUser.objects.filter(user=request.user).delete()
     next_url = request.GET.get('next', None) or utils.get_setting(
