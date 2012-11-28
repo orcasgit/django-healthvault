ac355007a0b9f20a05104036eb8f6c634891d5f5
Rebecca Lovewell <rebecca@caktusgroup.com>
2012-11-28 16:01:33 -0500

    Added docs directory to path

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@ import sys, os
 
 import django
 
+
+sys.path.append(os.path.dirname(__file__))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 if django.VERSION < (1, 4):
     from django.core.management import setup_environ
