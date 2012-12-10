5b0768ebec935f70526f4d949e37890579ed71a3
James Madson <jamesmadson@JAMESs-MacBook-Pro.local>
2012-12-10 14:09:28 -0800

    Wrapping up templates for Django/ORCAS HealthVault.

--- healthvaultapp/templates/django-healthvault-bootstrap/js/charts/line.js
+++ healthvaultapp/templates/django-healthvault-bootstrap/js/charts/line.js
@@ -1,20 +1,22 @@
 $(function () {
-    var sin = [], cos = [];
+    var syst = [], dias = [], pulse = [];
     for (var i = 0; i < 10; i += 0.5) {
-        sin.push([i, Math.sin(i)]);
-        cos.push([i, Math.cos(i)]);
+        syst.push([i, Math.syst(i)]);
+        dias.push([i, Math.dias(i)]);
+        pulse.push([i, Math.pulse(i);
     }
 
     var plot = $.plot($("#line-chart"),
-           [ { data: sin, label: "sin(x)"}, { data: cos, label: "cos(x)" } ], {
+           [ { data: syst, label: "syst(x)"}, { data: dias, label: "dias(x)"}, { data: pulse, label: "pulse(x)" } ], {
                series: {
-                   lines: { show: true },
+                   lines: { show: true,fill: true,
+            fillColor: { colors: [ { opacity: 0.6 }, { opacity: 0.2 } ] }, },
                    points: { show: true }
                },
                
                grid: { hoverable: true, clickable: true },
                yaxis: { min: -1.1, max: 1.1 },
 			   xaxis: { min: 0, max: 9 },
-    	colors: ["#008FC5", "#222", "#666", "#BBB"]
+    	colors: ["#008FC5", "#222", "#666", "#BBB", "#666", "#BBB"]
              });
 });
